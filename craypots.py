64490cb514acd6c41e3c000959439845cde9eab1
JorelBaywiss <joel.bariss@student.education.wa.edu.au>
2020-05-08 14:00:48 +0800

    <empty>

--- craypots.py
+++ craypots.py
@@ -1,1 +1,12 @@
 #craypots
+#Starting resources
+boats=1
+pots=5
+money=50
+#turn is defined here
+def turn():
+    #globalising variables to use
+    global boats
+    global pots
+    global money
+    
